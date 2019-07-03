24b9dc85c9b48e430205612cd5b3bad56cbf2fb9
munhyunsu <munhyunsu@gmail.com>
2019-07-03 11:39:40 +0900

    server script

---
+++ rest_server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+class ClientInfo(Resource):
+    def post(self):
+        data = request.get_json()
+        print(data)
+        return ''
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(ClientInfo, '/clientinfo')
+
+if __name__ == '__main__':
+    app.run(debug=True)
